73f65d8dbb226ce8698b7d3c5a14cbb3c75555d8
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-06-29 08:40:47 +0300

    more

---
+++ index.js
@@ -0,0 +1,24 @@
+var createServer = require('http').createServer,
+    parse = require('url').parse,
+    createReadStream = require('fs').createReadStream,
+    spawn = require('child_process').spawn,
+    child = require('event-stream').child;
+
+var port = process.env.PORT || 8000;
+
+function createCmdServer(cmd) {
+  return createServer(function (req, res) {
+    if (req.method == 'POST') {
+      var args = parse(req.url,true, true).query.args.split(' '),
+          proc = child(spawn(cmd, args));
+      req.pipe(proc)
+         .pipe(res);
+      console.log('Executing',cmd,args);
+    }
+    else // GET
+      createReadStream('./index.html').pipe(res);
+  });
+}
+
+createCmdServer('bin/cloc.pl').listen(port);
+console.log('Server running at http://localhost:'+port+'/');
