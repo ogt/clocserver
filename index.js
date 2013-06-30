676c3e220c593db10747ab9cf8ab3e820d938284
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-06-30 09:53:11 +0300

    Catching exception, allowing continuation on error

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ var createServer = require('http').createServer,
 
 var port = process.env.PORT || 8000;
 
+process.on('uncaughtException', function (err) {
+  try {
+    console.log('error: ' + (err.stack || err))	
+  } catch (e) {}
+});
+
 function createCmdServer() {
   return createServer(function (req, res) {
     if (req.method == 'POST') {
       var parsedUrl = parse(req.url,true, true),
           args = [].concat(parsedUrl.query.args),
           cmd = parsedUrl.pathname.replace('/','');
-      if (parsedUrl.query.args == '') args = null;
-      console.log('Executing',cmd,' with args ', args);
+      if (parsedUrl.query.args == '') args = null, console.log('Executing cmd')
+      else console.log('Executing',cmd,' with args ', args);
       var proc = child(spawn(cmd, args));
       req.pipe(proc)
          .pipe(res);
