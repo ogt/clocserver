98750fbd39a547a307245ecc38da1734fb401f61
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-07-15 20:29:28 -0700

    Updated using webcommand-express

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
+var app = require('webcommand-express')(['cloc']);
 
 var port = process.env.PORT || 8000;
+process.env['PATH'] = 'bin:' + process.env['PATH'];
 
+app.get('/', function(req,res) {
+    console.log(req.host);
+    res.sendfile('index.html');
+});
 
+app.listen(port);
+console.log('Listening on port: '+port);
 
-function fetchUrlToLocalFile(url, cb){
-  var temp = require('temp'),
-      request = require('request');
-  
-  var tmpStream = temp.createWriteStream();
-      req = request(url);
-  req.pipe(tmpStream);
-  req.on('end', function() { cb(err, tmpStream.path); });
-}
-
-/*
-function fetchUrls(urllist) {
-  var urlMap;
-  for (url in urlList) {
-    var p = promise();
-    fetchUrlToLocal
-  }
-}
-*/
-var createCommandServer = require('webcommand').createCommandServer;
-createCommandServer().listen(port);
-console.log('Server running at http://localhost:'+port+'/');
